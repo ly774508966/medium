2c0754f7d52004312c87dcac8680335039349feb
Amelie Rosser <amelierosser1986@gmail.com>
2017-02-12 15:55:26 +0000

    Removing uuid dep

--- src/geometry/BufferAttribute.js
+++ src/geometry/BufferAttribute.js
@@ -1,11 +1,9 @@
 import * as GL from 'core/GL';
-import uuidV1 from 'uuid/v1';
 
 let gl;
 
 export default class BufferAttribute {
 	constructor(type, data, itemSize, shaderAttribute = true) {
-		this.uuid = uuidV1();
 		this.type = type;
 		this.itemSize = itemSize;
 		this.numItems = data.length / itemSize;
