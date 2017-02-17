1ca59b9361388677d4392e813ae6be36d5f369a8
Amelie Rosser <amelierosser1986@gmail.com>
2017-02-17 23:54:03 +0000

    Added target and panning functionality

--- src/controls/OrbitControls.js
+++ src/controls/OrbitControls.js
@@ -1,26 +1,35 @@
 import {
 	clamp,
 } from 'math/Utils';
+import Vector3 from 'math/Vector3';
+
+const MODE_DRAG = 'MODE_DRAG';
+const MODE_PAN = 'MODE_PAN';
 
 export default class OrbitControls {
 	constructor(camera, element) {
-		const defaults = {
-			speed: 5,
-		};
-
+		this.rotationSpeed = 5;
+		this.panSpeed = 10;
 		this._camera = camera;
 		this._element = element;
 		this._zoomMin = 0.1;
 		this._zoomMax = Infinity;
 		this._radius = Math.max(camera.position.x, camera.position.z);
+		this._defaultRadius = Math.max(camera.position.x, camera.position.z);
 		this._rotationX = Math.atan2(camera.position.y - 0, this._radius - 0);
 		this._rotationY = Math.atan2(camera.position.z - 0, camera.position.x - 0);
+		this._defaultRotationX = Math.atan2(camera.position.y - 0, this._radius - 0);
+		this._defaultRotationY = Math.atan2(camera.position.z - 0, camera.position.x - 0);
 		this._offsetX = 0;
 		this._offsetY = 0;
+		this._offsetPanX = 0;
+		this._offsetPanY = 0;
+		this._target = new Vector3();
+		this._targetOffset = new Vector3();
+		this._direction = new Vector3();
+		this._up = new Vector3(0, 1, 0);
 		this._width = window.innerWidth;
 		this._height = window.innerHeight;
-
-		this.settings = Object.assign(defaults, {});
 
 		this._element.addEventListener('mousedown', this._onTouchStart, false);
 		this._element.addEventListener('mousemove', this._onTouchMove, false);
@@ -30,13 +39,28 @@ export default class OrbitControls {
 		this._element.addEventListener('touchend', this._onTouchEnd, false);
 		this._element.addEventListener('contextmenu', this._onContextMenu, false);
 		window.addEventListener('mousewheel', this._onMouseWheel, false);
+		window.addEventListener('keypress', this._onKeypress, false);
 	}
 
 	_onTouchStart = (event) => {
-		this._offsetY = this._rotationY;
-		this._offsetX = this._rotationX;
+		event.preventDefault();
+
+		switch (event.which) {
+			case 3:
+				this._mode = MODE_PAN;
+				this._offsetY = this._target.y;
+				this._offsetX = this._target.x;
+				break;
+			default: {
+				this._mode = MODE_DRAG;
+				this._offsetY = this._rotationY;
+				this._offsetX = this._rotationX;
+			}
+		}
+
 		this._startY = event.pageX / this._width;
 		this._startX = event.pageY / this._height;
+		this._targetOffset.copy(this._target);
 		this.isDown = true;
 	}
 
@@ -45,10 +69,22 @@ export default class OrbitControls {
 			const y = event.pageX / this._width;
 			const x = event.pageY / this._height;
 
-			this._rotationX = this._offsetX + -(this._startX - x) * this.settings.speed;
-			this._rotationY = this._offsetY + (this._startY - y) * this.settings.speed;
-
-			this._rotationX = clamp(this._rotationX, -Math.PI / 2, Math.PI / 2);
+			switch (this._mode) {
+				case MODE_PAN: {
+					this._direction.copy(this._camera.position).subtract(this._target).normalize();
+					const cross = this._direction.cross(this._up);
+					const tx = this._targetOffset.x + (this._startY - y) * this.panSpeed * cross.x;
+					const ty = this._targetOffset.y + -(this._startX - x) * this.panSpeed;
+					const tz = this._targetOffset.z + (this._startY - y) * this.panSpeed * cross.z;
+					this._target.set(tx, ty, tz);
+					break;
+				}
+				default: {
+					this._rotationX = this._offsetX + -(this._startX - x) * this.rotationSpeed;
+					this._rotationY = this._offsetY + (this._startY - y) * this.rotationSpeed;
+					this._rotationX = clamp(this._rotationX, -Math.PI / 2, Math.PI / 2);
+				}
+			}
 
 			this.update();
 		}
@@ -75,7 +111,8 @@ export default class OrbitControls {
 		const r = this._radius * Math.cos(this._rotationX); // radius of the sphere
 		const x = Math.sin(this._rotationY) * r;
 		const z = Math.cos(this._rotationY) * r;
-		this._camera.position.set(x, y, z);
+		this._camera.position.set(x, y, z).add(this._target);
+		this._camera.lookAt(this._target.x, this._target.y, this._target.z);
 	}
 
 	_onMouseWheel = (event) => {
@@ -92,6 +129,24 @@ export default class OrbitControls {
 		this._zoomConstraint(-delta);
 	}
 
+	_onKeypress = (event) => {
+		switch (event.keyCode) {
+			case 114: // r
+				// Reset
+				this.reset();
+				break;
+			default:
+		}
+	}
+
+	reset() {
+		this._target.set(0, 0, 0);
+		this._rotationY = this._defaultRotationY;
+		this._rotationX = this._defaultRotationX;
+		this._radius = this._defaultRadius;
+		this.update();
+	}
+
 	dispose() {
 		this._element.removeEventListener('mousedown', this._onTouchStart);
 		this._element.removeEventListener('mousemove', this._onTouchMove);
@@ -101,6 +156,7 @@ export default class OrbitControls {
 		this._element.removeEventListener('touchend', this._onTouchEnd);
 		this._element.removeEventListener('contextmenu', this._onContextMenu);
 		window.removeEventListener('mousewheel', this._onMouseWheel);
+		window.removeEventListener('keypress', this._onKeypress);
 		this._camera = null;
 		this._element = null;
 	}
