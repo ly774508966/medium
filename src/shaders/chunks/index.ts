75ec2b27e4f80356c22aeedb4068bf1a191f5d3b
Amelie Rosser <amelierosser1986@gmail.com>
2017-03-30 11:43:16 +0100

    Adding noise shader chunks

--- src/shaders/chunks/index.ts
+++ src/shaders/chunks/index.ts
@@ -1,27 +1,29 @@
-import EsVersion from './EsVersion.glsl';
-import Math from './Math.glsl';
 import Conditionals from './Conditionals.glsl';
 import * as DirectionalLights from './DirectionalLights.glsl';
+import EnvMapCube from './EnvMapCube.glsl';
+import EsVersion from './EsVersion.glsl';
+import Fog from './Fog.glsl';
+import Gamma from './Gamma.glsl';
+import Math from './Math.glsl';
+import * as Noise from './Noise.glsl';
 import Packing from './Packing.glsl';
 import * as PointLights from './PointLights.glsl';
-import Fog from './Fog.glsl';
 import ProjectionView from './ProjectionView.glsl';
-import Transpose from './Transpose.glsl';
-import EnvMapCube from './EnvMapCube.glsl';
-import Gamma from './Gamma.glsl';
 import * as Tonemap from './Tonemap.glsl';
+import Transpose from './Transpose.glsl';
 
 export default {
-	EsVersion,
-	Math,
 	Conditionals,
 	DirectionalLights,
+	EnvMapCube,
+	EsVersion,
+	Fog,
+	Gamma,
+	Math,
+	Noise,
 	Packing,
 	PointLights,
-	Fog,
 	ProjectionView,
-	Transpose,
-	EnvMapCube,
-	Gamma,
 	Tonemap,
+	Transpose,
 };
