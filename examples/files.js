86748beb90dd89fafe9f718114af81729a21b72f
Amelie Rosser <amelierosser1986@gmail.com>
2017-04-17 16:11:01 +0100

    Using dir variable

--- examples/files.js
+++ examples/files.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const dir = path.join(__dirname, 'src/js');
 const demos = [];
-fs.readdir(path.join(__dirname, 'src/js'), (err, files) => {
+fs.readdir(dir, (err, files) => {
 	files.forEach(file => {
 		if (fs.lstatSync(path.join(dir, file)).isDirectory()) {
 			demos.push(file.split('.js')[0]);
