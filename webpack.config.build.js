61b6623a91b6452d06aa4a63d0f8d5931ec5fc10
Amelie Rosser <amelierosser1986@gmail.com>
2017-09-06 23:27:40 +0100

    Prettier

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,49 +1,49 @@
-const path = require('path');
-const BabiliPlugin = require('babili-webpack-plugin');
+const path = require("path");
+const BabiliPlugin = require("babili-webpack-plugin");
 
-const production = process.env.NODE_ENV === 'production';
+const production = process.env.NODE_ENV === "production";
 
 module.exports = {
-  entry: path.join(process.cwd(), 'src/index.ts'),
+  entry: path.join(process.cwd(), "src/index.ts"),
   output: {
-    path: path.join(process.cwd(), 'lib'),
-    filename: production ? 'ixviii.medium.min.js' : 'ixviii.medium.js',
-    libraryTarget: 'commonjs2'
+    path: path.join(process.cwd(), "lib"),
+    filename: production ? "ixviii.medium.min.js" : "ixviii.medium.js",
+    libraryTarget: "commonjs2"
   },
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: [".ts", ".js"]
   },
   module: {
     loaders: [
       {
-        test: /\.ts(x?)$/,
+        test: /\.ts$/,
         exclude: /node_modules/,
-        loader: 'babel-loader!ts-loader'
+        loader: "babel-loader!ts-loader"
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
           presets: [
             [
-              'env',
+              "env",
               {
                 targets: {
-                  browsers: ['last 2 versions', 'ios_saf >= 8', 'not IE <= 10']
+                  browsers: ["last 2 versions", "ios_saf >= 8", "not IE <= 10"]
                 }
               }
             ]
           ],
-          plugins: ['babel-plugin-transform-class-properties']
+          plugins: ["babel-plugin-transform-class-properties"]
         }
       },
       {
         test: /\.json$/,
-        loader: 'json-loader'
+        loader: "json-loader"
       }
     ]
   },
-  stats: 'minimal',
+  stats: "minimal",
   plugins: production ? [new BabiliPlugin()] : []
 };
