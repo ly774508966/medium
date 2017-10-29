2aa4aeb8dfa35808e8e4d68c2258950d62347566
Amelie Rosser <amelierosser1986@gmail.com>
2017-10-29 13:16:47 +0000

    Fixing error for webgl1

--- src/core/RenderTarget.ts
+++ src/core/RenderTarget.ts
@@ -53,13 +53,10 @@ export default class RenderTarget {
     gl.bindFramebuffer(gl.FRAMEBUFFER, this.frameBuffer);
     this.texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, this.texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    gl.texParameteri(
-      gl.TEXTURE_2D,
-      gl.TEXTURE_MIN_FILTER,
-      gl.LINEAR_MIPMAP_NEAREST
-    );
-    // gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texImage2D(
       gl.TEXTURE_2D,
       0,
@@ -197,7 +194,6 @@ export default class RenderTarget {
     });
 
     gl.bindTexture(gl.TEXTURE_2D, this.texture);
-    gl.generateMipmap(gl.TEXTURE_2D);
     gl.bindTexture(gl.TEXTURE_2D, null);
 
     // Reset
