b5dbc77b2fa6d02ec12aaf93893e76d9a93e7b90
Amelie Rosser <amelierosser1986@gmail.com>
2017-11-01 19:07:49 +0000

    Using examples json

--- pug.config.js
+++ pug.config.js
@@ -1,18 +1,6 @@
 const shell = require('shelljs');
-const fs = require('fs');
-const path = require('path');
-
-const dir = path.join(__dirname, 'examples/src/js');
-const demos = [];
-shell.ls(`${dir}`).forEach(file => {
-  if (fs.lstatSync(path.join(dir, file)).isDirectory()) {
-    demos.push(file.split('.js')[0]);
-  }
-});
-
-const obj = JSON.stringify({
-  demos,
-});
+const pkg = require('./package.json');
+const examples = require('./examples/examples.json');
 
 const production = process.env.NODE_ENV === 'production';
 
@@ -29,7 +17,12 @@ if (exampleDir !== undefined) {
   entry = `./examples/src/templates/${exampleDir}.pug`;
 }
 
-const cmd = `pug --silent --obj '${obj}' ${watch} ${entry} --out ./examples`;
+const obj = JSON.stringify({
+  description: pkg.description,
+  examples
+});
+
+const cmd = `pug --silent --obj '${obj}' ${watch} ${entry} --pretty --out ./examples`;
 
 shell.exec(cmd, code => {
   if (code !== 0) {
